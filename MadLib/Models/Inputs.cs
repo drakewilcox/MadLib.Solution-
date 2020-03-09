6d863a476383319d7242e3d673d338592f4b0e46
Student <example@example.com>
2020-03-09 11:27:08 -0700

    add input fields.

--- MadLib/Models/Inputs.cs
+++ MadLib/Models/Inputs.cs
@@ -2,6 +2,12 @@ namespace MadLib.Models
 {
     public class Inputs
     {
-        
+        public string Name1 { get; set; }
+        public string Name2 { get; set; }
+        public string Animal { get; set; }
+        public string Exclamation { get; set; }
+        public string Verb { get; set; }
+        public string Noun { get; set; }
+
     }
 }
