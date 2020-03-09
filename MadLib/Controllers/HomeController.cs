9d538a7f49a5a304cebd657feb340e91d4510a3a
Student <example@example.com>
2020-03-09 13:54:36 -0700

    added 2 new routes for new madlib pages

--- MadLib/Controllers/HomeController.cs
+++ MadLib/Controllers/HomeController.cs
@@ -1,25 +1,57 @@
 using Microsoft.AspNetCore.Mvc;
 using MadLib.Models;
 namespace MadLib.Controllers
-
 {
   public class HomeController : Controller
   {
     [Route("/")]
+    public ActionResult Welcome() { return View(); }
+
+    [Route("/form")]
     public ActionResult Form() { return View(); }
-    [Route("/madlib")]
+
+    [Route("/horror")]
+    public ActionResult Horror() { return View(); }
+
+    [Route("/underwater")]
+    public ActionResult UnderWater() { return View(); }
+
+    [Route("/madlib")]                    
     public ActionResult MadLib(string name1, string name2, string animal, string exclamation, string verb, string noun)
     {
       Inputs myInputs = new Inputs();
       myInputs.Name1 = name1;
       myInputs.Name2 = name2;
-      myInputs.Animal = animal;
+      myInputs.Animal = animal;              
       myInputs.Exclamation = exclamation;
       myInputs.Verb = verb;
       myInputs.Noun = noun;
       return View(myInputs);
     } 
-
+     [Route("/madlib2")]                    
+    public ActionResult MadLib2(string name1, string name2, string animal, string exclamation, string verb, string noun)
+    {
+      Inputs myInputs = new Inputs();
+      myInputs.Name1 = name1;
+      myInputs.Name2 = name2;
+      myInputs.Animal = animal;              
+      myInputs.Exclamation = exclamation;
+      myInputs.Verb = verb;
+      myInputs.Noun = noun;
+      return View(myInputs);
+    } 
+     [Route("/madlib3")]                    
+    public ActionResult MadLib3(string name1, string name2, string animal, string exclamation, string verb, string noun)
+    {
+      Inputs myInputs = new Inputs();
+      myInputs.Name1 = name1;
+      myInputs.Name2 = name2;
+      myInputs.Animal = animal;              
+      myInputs.Exclamation = exclamation;
+      myInputs.Verb = verb;
+      myInputs.Noun = noun;
+      return View(myInputs);
+    } 
   }
 
 
